4397fc002bac80789bf68d8fc153dec39a038b93
Lucas Trochet <l.trochet25@gmail.com>
2018-12-06 11:26:30 +0100

    Add default structure + builder command

---
+++ build_crontab.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Create backup in case something goes wrong while generating new crontab
+crontab -l > /tmp/backup.cron
+
+env > /tmp/crontab;
+cat ./crontab >> /tmp/crontab;
+
+crontab -u $USER /tmp/crontab;
